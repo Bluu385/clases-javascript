476a6a0fe07005a3ffe6dcc588f433102e938e7f
Bluu385 <98234361+Bluu385@users.noreply.github.com>
2022-01-22 13:47:29 -0600

    Add files via upload

---
+++ clase03.js
@@ -0,0 +1,7 @@
+var miEdad=16;
+var edadFalsa=32;
+if(miEdad<edadFalsa){
+   console.log("la var miEdad es menor a edadFalsa")
+}else{
+console.log("la var edadFalsa es mayor a miEdad")
+}
---
+++ clases01.js
@@ -0,0 +1,8 @@
+var A=10;
+var B=20;
+if(A<B){
+  console.log("la variable A es menor a la B")
+}else{
+  console.log("la variable B es mayor a la A")
+}
+ 
